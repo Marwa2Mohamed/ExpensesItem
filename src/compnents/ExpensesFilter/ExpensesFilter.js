ef713cc174f121b25a553e2da9966845012fb9a0
Marwa2Mohamed <marwam.2@hotmail.com>
2021-07-26 21:50:03 -0700

    eventhandler and onprops are added

--- src/compnents/ExpensesFilter/ExpensesFilter.js
+++ src/compnents/ExpensesFilter/ExpensesFilter.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import { useState } from "react";
 
-import './ExpensesFilter.css';
+import "../CSS/ExpensesFilter.css";
 
-const ExpensesFilter = () => {
+const ExpensesFilter = (props) => {
+   // eslint-disable-next-line
+  const [Year,setNewYear] = useState('2022');
+  const yearFilter = (event) => {
+    let newYear = event.target.value;
+    setNewYear(newYear);
+    props.onChoice(newYear);
+  }
   return (
-    <div className='expenses-filter'>
-      <div className='expenses-filter__control'>
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select>
-          <option value='2022'>2022</option>
-          <option value='2021'>2021</option>
-          <option value='2020'>2020</option>
-          <option value='2019'>2019</option>
+        <select onChange = {yearFilter}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
         </select>
       </div>
     </div>
