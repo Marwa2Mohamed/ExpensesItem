07e60d39f6c547a0a7a4b036112b6490e5b9d509
Marwa2Mohamed <marwam.2@hotmail.com>
2021-07-28 03:15:41 -0700

    used the state update to update the select value

--- src/compnents/ExpensesFilter/ExpensesFilter.js
+++ src/compnents/ExpensesFilter/ExpensesFilter.js
@@ -3,18 +3,18 @@ import { useState } from "react";
 import "../CSS/ExpensesFilter.css";
 
 const ExpensesFilter = (props) => {
-   // eslint-disable-next-line
-  const [Year,setNewYear] = useState('2022');
+  // eslint-disable-next-line
+  const [Year, setNewYear] = useState("2022");
   const yearFilter = (event) => {
     let newYear = event.target.value;
     setNewYear(newYear);
     props.onChoice(newYear);
-  }
+  };
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select onChange = {yearFilter}>
+        <select value={Year} onChange={yearFilter}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
