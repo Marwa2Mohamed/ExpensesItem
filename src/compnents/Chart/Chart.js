befb03c69e41da798573acf19d7470c9176d5ab3
Marwa2Mohamed <marwam.2@hotmail.com>
2021-08-16 15:41:50 -0700

    Finished building the chart and chartBar components' structure

--- src/compnents/Chart/Chart.js
+++ src/compnents/Chart/Chart.js
@@ -2,7 +2,19 @@ import "../CSS/Chart.css";
 import ChartBar from "./ChartBar";
 
 function Chart(props) {
-  
+  //need to create 12 dynamically chart bars
+  return (
+    <div>
+      {props.ChartBarArray.map((bar) => (
+        <ChartBar
+          key={bar.label}
+          value={bar.value}
+          maxValue={null}
+          label={bar.label}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Chart;
